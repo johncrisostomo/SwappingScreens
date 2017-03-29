0735523172f4c2db86969b2050a625a8ff1b89cb
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-29 20:45:49 +0800

    Initial commit

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by John Crisostomo on 29/03/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
